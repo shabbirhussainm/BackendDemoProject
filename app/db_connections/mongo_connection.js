c00cd24c4d8f664cd3f7d32a0c75b3ce19bc0867
shabbirhussainm <shabbirhussainm04@gmail.com>
2021-10-26 15:56:54 +0000

    Demo Project files

---
+++ app/db_connections/mongo_connection.js
@@ -0,0 +1,36 @@
+/**
+ * Created by web on 11/2/17.
+ */
+var mongoose = require('mongoose');
+var config = require('config');
+var log = require('../logger/logger');
+mongoose.Promise = global.Promise;
+
+
+exports.mongoConnection = function () {
+    mongoose.connect('mongodb://' + config.database.mongo.host + '/' + config.database.mongo.name, { useMongoClient: true });
+
+    //mongoose.connect('mongodb://127.0.0.1:27017/mongo' + config.database.mongo.host + '/' + config.database.mongo.name);
+
+    mongoose.connection.on('connected', function () {
+        log.info('Mongoose default connection open to ' + 'mongodb://' + config.database.mongo.host + '/' + config.database.mongo.name);
+    });
+
+    mongoose.connection.on('error', function (err) {
+        log.info('Mongoose default connection error: ' + err);
+    });
+
+
+    mongoose.connection.on('disconnected', function () {
+        log.info('Mongoose default connection disconnected');
+    });
+
+
+    process.on('SIGINT', function () {
+        mongoose.connection.close(function () {
+            log.info('Mongoose default connection disconnected through app termination');
+            process.exit(0);
+        });
+    });
+};
+
